e2b152beaeea12ea5a9c08666b1ea250494eddba
セリカ・シルフィル <64072399+natsumerinchan@users.noreply.github.com>
2023-11-26 17:23:10 +0800

    Update build_kernel.sh
    Co-Authored-By: セリカ・シルフィル <64072399+natsumerinchan@users.noreply.github.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #设置环境
 
+KERNEL_PATH=$PWD
+
 # Special Clean For Huawei Kernel.
 if [ -d include/config ];
 then
@@ -13,7 +15,7 @@ fi
 echo " "
 echo "***Setting environment...***"
 # 交叉编译器路径
-export PATH=$PATH:/home/coconutat/github/android_kernel_huawei_florida/aarch64-linux-android-4.9/bin
+export PATH=$PATH:$KERNEL_PATH/aarch64-linux-android-4.9/bin
 export CROSS_COMPILE=aarch64-linux-android-
 
 export GCC_COLORS=auto
@@ -36,16 +38,15 @@ date="$(date +%Y.%m.%d-%I:%M)"
 echo "***Building Kernel...***"
 make ARCH=arm64 O=out $EV merge_hi6250_mod_defconfig
 # 定义编译线程数
-make ARCH=arm64 O=out $EV -j256 2>&1 | tee kernel_log-${date}.txt
+make ARCH=arm64 O=out $EV -j`nproc` 2>&1 | tee kernel_log-${date}.txt
 
 #打包内核
 
 if [ -f out/arch/arm64/boot/Image.gz ];
 then
 	echo "***Packing kernel...***"
-	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x00400000 --cmdline "loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x10000000 --os_version 9 --os_patch_level 2019-05-05 --output Kirin659_V"$v"_${date}.img
-	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x00400000 --cmdline "loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x10000000 --os_version 9 --os_patch_level 2019-05-05 --output Kirin659_V"$v"_PM_${date}.img
-	cp out/arch/arm64/boot/Image.gz Image.gz 
+	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x00400000 --cmdline "loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x10000000 --os_version 9 --os_patch_level 2019-05-05 --output out/arch/arm64/boot/Kirin659_V"$v"_${date}.img
+	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x00400000 --cmdline "loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x10000000 --os_version 9 --os_patch_level 2019-05-05 --output out/arch/arm64/boot/Kirin659_V"$v"_PM_${date}.img
 	echo " "
 	echo "***Sucessfully built kernel...***"
 	echo " "
